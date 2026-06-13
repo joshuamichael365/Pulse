6b4e2e5ffab6273110071ad334abcbe21f80bdde
jmmicha4 <jmmicha4@ncsu.edu>
2026-06-12 20:37:39 -0400

    Initial Commit

---
+++ PulseTests/PulseTests.swift
@@ -0,0 +1,19 @@
+//
+//  PulseTests.swift
+//  PulseTests
+//
+//  Created by Joshua Michael on 6/12/26.
+//
+
+import Testing
+@testable import Pulse
+
+struct PulseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
